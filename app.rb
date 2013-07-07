18f236e55ac952d5f53a3895261ecf2eea7c87b5
Minjie Zha <minjiezha@gmail.com>
2013-07-08 05:04:06 +0800

    add index page

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require 'sinatra'
 
-#before '/*' do
-#  set :haml, {:layout => :layout, :format => :html5}
-#end
-
 set :haml, {:format => :html5, :layout => :layout}
 
+
 get '/' do
+  @providers = []
+  Dir.glob(File.join('providers', '*.yml')) do |p|
+    name = File.basename(p, '.yml')
+    if name.eql?('twitter')
+      twitter = {}
+      twitter[:name] = name
+      twitter[:path] = '/auth/twitter'
+
+      @providers << twitter
+      @providers << twitter
+    end
+  end
   haml :index
 end
