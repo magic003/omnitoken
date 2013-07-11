5db63f1067a45da79195570e53d4f902f6c8fab8
Minjie Zha <minjiezha@gmail.com>
2013-07-11 10:48:36 +0800

    refactor app.rb

--- app.rb
+++ app.rb
@@ -1,20 +1,28 @@
-require 'sinatra'
+require 'sinatra/base'
 
 $:.unshift(File.dirname(__FILE__) + '/lib') unless
   $:.include?(File.dirname(__FILE__) + '/lib') || $:.include?(File.expand_path(__FILE__ + '/lib'))
 
 require 'get_token'
 
-set :haml, {:format => :html5, :layout => :layout}
+class GetTokenApp < Sinatra::Base
+  set :haml, {:format => :html5, :layout => :layout}
 
-
-get '/' do
-  @providers = []
-  Dir.glob(File.join('providers', '*.yml')) do |p|
-    name = File.basename(p, '.yml')
-    if GT::Registry.instance[name]
-      @providers << GT::Registry.instance[name]
+  def initialize(app=nil)
+    super(app)
+    @providers = []
+    Dir.glob(File.join('providers', '*.yml')) do |p|
+      name = File.basename(p, '.yml')
+      if GT::Registry.instance[name]
+        @providers << GT::Registry.instance[name]
+      end
     end
   end
-  haml :index
+
+  get '/' do
+    haml :index
+  end
+
+  run! if app_file == $0
+
 end
