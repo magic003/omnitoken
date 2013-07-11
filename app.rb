025555457ff6d177023c304f7dae5d39261017ba
Minjie Zha <minjiezha@gmail.com>
2013-07-11 11:14:15 +0800

    a very early verion which makes getting twitter token work

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require 'omniauth'
+require 'omniauth-twitter'
 
 $:.unshift(File.dirname(__FILE__) + '/lib') unless
   $:.include?(File.dirname(__FILE__) + '/lib') || $:.include?(File.expand_path(__FILE__ + '/lib'))
@@ -6,6 +8,7 @@ $:.unshift(File.dirname(__FILE__) + '/lib') unless
 require 'get_token'
 
 class GetTokenApp < Sinatra::Base
+  enable :sessions
   set :haml, {:format => :html5, :layout => :layout}
 
   def initialize(app=nil)
@@ -15,6 +18,13 @@ class GetTokenApp < Sinatra::Base
       name = File.basename(p, '.yml')
       if GT::Registry.instance[name]
         @providers << GT::Registry.instance[name]
+      end
+    end
+
+    providers = @providers
+    self.class.use OmniAuth::Builder do
+      providers.each do |p|
+        provider p.id.to_sym, "S2DJqwT9eer6urLLkLf9A", "x4tALNCgHR1zX9ko1yGARTlQBlQED7OWJOp0F9LHZ0"
       end
     end
   end
